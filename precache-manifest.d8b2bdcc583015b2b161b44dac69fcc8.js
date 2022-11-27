603b8de41221395d092a50324c0c7f50a916237c
Blair Mottau <blairmottau@gmail.com>
2022-11-27 20:55:21 +1100

    Updates

---
+++ precache-manifest.d8b2bdcc583015b2b161b44dac69fcc8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1f4a9ec9c01dcc50b10",
+    "url": "/casual-fan-website/static/css/main.ff56c729.chunk.css"
+  },
+  {
+    "revision": "d1f4a9ec9c01dcc50b10",
+    "url": "/casual-fan-website/static/js/main.d1f4a9ec.chunk.js"
+  },
+  {
+    "revision": "1b3f48c6de62d66542ce",
+    "url": "/casual-fan-website/static/js/1.1b3f48c6.chunk.js"
+  },
+  {
+    "revision": "cea9e48df1f719f54f30",
+    "url": "/casual-fan-website/static/js/2.cea9e48d.chunk.js"
+  },
+  {
+    "revision": "f77240fc7f5d9c2e1c4b",
+    "url": "/casual-fan-website/static/js/runtime~main.f77240fc.js"
+  },
+  {
+    "revision": "774f3fa1b3045e325a415b1d99aff105",
+    "url": "/casual-fan-website/static/media/IDATD.774f3fa1.jpg"
+  },
+  {
+    "revision": "5b2bf99b0e30051b059b4d92c5514e8f",
+    "url": "/casual-fan-website/static/media/CFLogo.5b2bf99b.png"
+  },
+  {
+    "revision": "9e4af06e3a6476e35cb5ffd60a0b7985",
+    "url": "/casual-fan-website/static/media/live-vid.9e4af06e.JPG"
+  },
+  {
+    "revision": "3c0931a7018ca9154db860ba4fbabe53",
+    "url": "/casual-fan-website/static/media/press1.3c0931a7.jpg"
+  },
+  {
+    "revision": "5d1ab57c725bee7f09a1c61216dae675",
+    "url": "/casual-fan-website/static/media/press2.5d1ab57c.jpg"
+  },
+  {
+    "revision": "cfaf94e45ede96dd064a2d314596bf92",
+    "url": "/casual-fan-website/index.html"
+  }
+];
